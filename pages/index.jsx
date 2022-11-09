8208cc57279287efa81700b7245033e7fd8db625
Stefanie <stefcodes@gmail.com>
2022-11-09 15:28:42 -0800

    add use state hook for wallet and collection

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 
 const Home = () => {
+  const [wallet, setWalletAddress] = useState("");
+  const [collection, setCollectionAddress] = useState("");
  
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input type={"text"} placeholder="Add your wallet address"></input>
-        <input type={"text"} placeholder="Add the collection address"></input>
+        <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
       </div>
     </div>
   )
