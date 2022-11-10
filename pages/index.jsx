6401b621567462da03bf6a3b3f0a54807f45279a
Stefanie <stefcodes@gmail.com>
2022-11-09 20:55:54 -0800

    get NFTs

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,11 @@ import { useState } from 'react'
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
+  const [NFT, setNFTs] = useState([])
  
+
+  
+
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
@@ -12,7 +16,6 @@ const Home = () => {
         <label className="text-grey-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
-
           }
         }>Let's Go! </button>
       </div>
