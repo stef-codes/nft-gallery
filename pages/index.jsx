9c12974cd5e9588751a16c7950e62edf0015f89c
Stefanie <stefcodes@gmail.com>
2022-11-09 20:25:54 -0800

    add button and remove previous code

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,10 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
+        <input type={"text"} placeholder="Add your wallet address"></input>
+        <input type={"text"} placeholder="Add the collection address"></input>
+        <label className="text-grey-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}>Let's Go! </button>
       </div>
     </div>
   )
