562c1f9d9eacd769b6cfe08f30d58a35a130a10d
Stefanie <stefcodes@gmail.com>
2022-11-09 20:32:27 -0800

    add on click handler

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,9 +8,13 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input type={"text"} placeholder="Add your wallet address"></input>
-        <input type={"text"} placeholder="Add the collection address"></input>
+        <input type={"text"} placeholder="Add collection address"></input>
         <label className="text-grey-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
-        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}>Let's Go! </button>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
+          () => {
+
+          }
+        }>Let's Go! </button>
       </div>
     </div>
   )
