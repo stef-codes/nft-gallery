a9b1ec2af14668d003ed4cd41348a623b57e0578
Stefanie <stefcodes@gmail.com>
2022-11-11 22:55:57 -0800

    add onChange handler for collections

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,7 +55,7 @@ const Home = () => {
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
         <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
-        <label className="text-grey-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
+        <label className="text-grey-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
           }
