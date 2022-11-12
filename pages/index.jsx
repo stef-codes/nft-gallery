3b9b3bbb47d4b554461b3d2db1ebaa53f00ea0a6
Stefanie <stefcodes@gmail.com>
2022-11-11 20:51:02 -0800

    add styling and api abstraction

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts"); 
     const api_key = "owZbZwB9hD1wDTBhsp74i5xpaXIzTBMb"
-    const baseURL = `https://eth-mainnet.g.alchemy.com/v2/owZbZwB9hD1wDTBhsp74i5xpaXIzTBMb`;
+    const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
     };
@@ -35,8 +35,8 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input type={"text"} placeholder="Add your wallet address"></input>
-        <input type={"text"} placeholder="Add collection address"></input>
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
         <label className="text-grey-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
